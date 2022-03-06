2632df5cf217c00cecc9d1b1776c7a311115e040
david77 <69095207+davve77@users.noreply.github.com>
2022-03-06 21:40:27 +0200

    Create plugin.bwrd.js

---
+++ plugins/plugin.bwrd.js
@@ -0,0 +1,11 @@
+/*
+    @name My First Plugin
+    @version 1.0.0
+    @description My cool plugin.
+    @author You
+    @source https://raw.githubusercontent.com/davve77/BetterWRD-Plugins/main/plugins/plugin.bwrd.js
+*/
+
+
+// Your plugin's script:
+console.log("Hello!");
