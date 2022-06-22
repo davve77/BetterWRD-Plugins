59264ce1bbdd349a52a4bdb01dfe7d56d0f6a5cd
david77 <69095207+davve77@users.noreply.github.com>
2022-06-23 00:28:52 +0300

    Update originalPoster.bwrd.js

--- plugins/originalPoster.bwrd.js
+++ plugins/originalPoster.bwrd.js
@@ -23,8 +23,8 @@ if(document.querySelector('.replygroup')){
     }
     .opDiv {
         font-size: 14px;
-        background: #8dc5ff;
-        color: black;
+        background: #93c8ff29;
+        color: white;
         padding: 0 5px;
         border-radius: 5px;
         display: flex;
@@ -33,7 +33,7 @@ if(document.querySelector('.replygroup')){
         gap: 2px;
     }
     .opDiv p{
-        font-weight: 600;
+        font-weight: 300;
     }
     `)
 
@@ -42,7 +42,7 @@ if(document.querySelector('.replygroup')){
         
         let opDiv = document.createElement('div')
         opDiv.setAttribute('class', 'opDiv')
-        opDiv.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 0 24 24" width="16" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"></path></svg> <p>OP</p>`
+        opDiv.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 0 24 24" width="16" fill="white"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"></path></svg> <p>OP</p>`
         post.firstElementChild.appendChild(opDiv)
         post.firstElementChild.classList.add('opName')
     })
